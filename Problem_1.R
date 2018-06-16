e29f2305823d0bfcecd595958d799236ac39f6a6
amarov <boyko.amarov@gmail.com>
2018-06-16 19:12:15 +0300

    Add solutions

--- Problem_1.R
+++ Problem_1.R
@@ -8,20 +8,75 @@ str(houseWork)
 
 ## a)
 
+table(houseWork$sex)
+## or 
+summary(houseWork$sex)
+
 ## b)
+
+summarise(group_by(houseWork, sex),
+          averageHours = mean(hours),
+          )
 
 ## c)
 
+houseWork <- within(houseWork, {
+  female <- sex == 'f'
+  male <- sex == 'm'
+})
+
 ## d)
+
+fit <- lm(hours ~ female, data = houseWork)
 
 ## e)
 
+summary(fit)
+
+## On average, men worked (in the house) 32.8 hours per week
+## Women worked (in the house) on average 14.45 hours less than men
+
 ## f)
 
+## H0: mu_f <= mu_m <=> mu_f- mu_m <= 0
+## beta1 = mu_f - mu_m (due to the coding of the variable female which is TRUE (1) for women and FALSE (0) for men)
+## it follows that H0: beta1 <= 0
+
 ## g)
+## Test-statistic is (from the regression output )
+## t = -45.38
+## the p-value of the test is
+n <- nrow(houseWork)
+1 - pt(-45.38, df = n - 2)
+## because the large values of the test-statistic go against the 
+## null hypothesis
 
 ## h)
+## We do not reject the null hypothesis, because the p-value calculated
+## above is larger than 0.05.
 
 ## i)
+## The t-test assumes that either that the hours spent in housework are
+## normally distributed in each group (men, women) and that the 
+## variance of hours is the same in the two groups (homoskedasticity)
+
+## The normality assumption is less problematic here because we have large sample sizes (see a)) we can rely on the central limit
+
+summarise(group_by(houseWork, sex),
+          sdHours = var(hours)
+          )
+
+## The equality of variance assumption however appears to be problematic
+## because the estimated variance of hours for men is roughly two times
+## larger than the estimated variance for women.
 
 ## j)
+## The points for this question will be substracted from
+## the required score count
+
+lm(hours ~ female + male, data = houseWork)
+
+## The coefficient for male cannot be estimated because
+## of multicollinearity: the dummy variables female and male add up to 1
+## for each observation and their sum is thus equal to the variable 
+## corresponding to the intercept.
--- Problem_2.R
+++ Problem_2.R
@@ -4,10 +4,62 @@ str(crime)
 
 ## a)
 
+fit <- lm(C ~ HS, data = crime)
+summary(fit)
+
+## The estimated coefficient for HS equals 1.48. This means that
+## we expect counties that differ by one percentage point
+## in high school graduates to have a difference of 1.48 crimes per 1000 persons.
+## and counties with more high school graduates to have higher crime rates 
+## (because the coefficient is positive)
+
+min(crime$HS)
+## The intercept corresponds to the expected crime rate in a county
+## with no high school graduates and is not a meaningful estimate
+## because it is very far away from any observed county (the smallest
+## observed share of high-school graduates is 54.5%!)
+
 ## b)
+## The points for this question will be substracted from
+## the required score count
+pairs(~ C + U  + I + HS, data = crime)
 
 ## c)
 
+fit1 <- lm(C ~ HS + U, data = crime)
+summary(fit1)
+
+## We see that the coefficient for HS is negative and not significantly 
+## different from 0 (see the t-test p-value).
+
+## This happens because the more HS and U are positively correlated
+## (see the scatterplot) and
+## because the level of urbanisation is positively correlated with 
+## the crime rate.
+
+## Accounting for the level of urbanisation there is no significant
+## (linear) association between crime rates and the share of high 
+## school graduates.
+
 ## d)
 
+## The model from a) reveals a positive (linear) association between
+## the share of high school graduates and crime rates. However, it does not
+## imply causation. The model from c) shows that the entire effect of HS
+## on crime rates is accounted for by the level of urbanisation.
+## Therefore the model from a) does not in any case support the polititian's 
+## proposition.
+
 ## e)
+## The points for this question will be substracted from
+## the required score count
+
+fit2 <- lm(C ~ HS + U + I, data = crime)
+fit3 <- lm(C ~ U, data = crime)
+
+anova(fit2, fit3)
+
+## The null hypothesis that both coefficients
+## are simultaneously equal to 0 cannot be rejected here
+## because the p-value = 0.43 > 0.05.
+
